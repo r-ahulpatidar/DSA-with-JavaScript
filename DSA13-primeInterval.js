a15c4803a611d0d3d98bf086c195efcba93eba53
Rahul <12qrahul@gmail.com>
2023-05-10 18:03:22 +0530

    dsa13 added

---
+++ DSA13-primeInterval.js
@@ -0,0 +1,21 @@
+// Program to Print All Prime Numbers in an Interval
+
+function primeNumber(lowerNumber, higherNumber) {
+  for (let i = lowerNumber; i <= higherNumber; i++) {
+    let flag = 0;
+
+    // looping through 2 to user input number
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        flag = 1;
+        break;
+      }
+    }
+
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && flag == 0) {
+      console.log(i);
+    }
+  }
+}
+primeNumber(2, 10);
