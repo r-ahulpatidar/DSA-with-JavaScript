5e20a030cd43be1b2a92fd9159693e6b9c2d9b2f
Rahul <12qrahul@gmail.com>
2023-06-19 07:41:45 +0530

    dsa41 added

---
+++ DSA41-removePropFromObject.js
@@ -0,0 +1,22 @@
+// Program to Remove a Property from an Object
+
+// creating an object
+const student = { 
+    name: 'John',
+    age: 20,
+    hobbies: ['reading', 'games', 'coding'],
+    greet: function() {
+        console.log('Hello everyone.');
+    },
+    score: {
+        maths: 90,
+        science: 80
+    }
+};
+
+// deleting a property from an object
+delete student.greet;
+delete student['score'];
+delete student['age'];
+
+console.log(student);
