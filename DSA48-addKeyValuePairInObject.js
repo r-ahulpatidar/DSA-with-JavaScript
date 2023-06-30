b6f2bf0314dac80af0d4da6bd6b7395411226119
Rahul <12qrahul@gmail.com>
2023-06-30 09:02:14 +0530

    dsa48 dsa49 dsa50 added

---
+++ DSA48-addKeyValuePairInObject.js
@@ -0,0 +1,12 @@
+// JavaScript Program to Add Key/Value Pair to an Object
+
+const person = {
+  name: "Monica",
+  age: 22,
+  gender: "female",
+};
+
+// add a key/value pair
+person.height = 5.4; // person['height'] = 5.4;
+
+console.log(person);
