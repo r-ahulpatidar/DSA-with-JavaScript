6e91096ac38a6e93217daba86df2c2f8ae5b14e6
Rahul <12qrahul@gmail.com>
2023-07-08 08:30:48 +0530

    dsa55 added

---
+++ DSA55-convertObjectToString.js
@@ -0,0 +1,19 @@
+// JavaScript Program to Convert Objects to Strings
+
+const person = {
+    name: 'Jack',
+    age: 27
+}
+
+const result =  JSON.stringify(person);
+// const result1 = String(person);
+// const result2 = String(person['name']);
+
+// console.log(result1);
+// console.log(result2);
+
+// console.log(typeof result1);
+
+
+console.log(result);
+console.log(typeof result);
