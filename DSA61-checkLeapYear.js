633f58b315cb7ab7d169cae9c692162f4d9d4373
Rahul <12qrahul@gmail.com>
2023-07-17 08:46:16 +0530

    dsa61 added

---
+++ DSA61-checkLeapYear.js
@@ -0,0 +1,13 @@
+// JavaScript Program to Check Leap Year
+
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+checkLeapYear(2001);
