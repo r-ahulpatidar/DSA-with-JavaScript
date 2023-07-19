4f8ff55c3857d012af8c995cd8968d07c71c845f
Rahul <12qrahul@gmail.com>
2023-07-19 09:02:52 +0530

    dsa63 added

---
+++ DSA63-displayCurrentDate.js
@@ -0,0 +1,16 @@
+// Javascript Program to Display Current Date
+
+// get local machine date time
+const date = new Date();
+
+// get the date as a string
+const n = date.toDateString();
+
+// get the time as a string
+const time = date.toLocaleTimeString();
+
+// display date
+console.log('Date: ' + n);
+
+// display time
+console.log('Time: ' + time);
